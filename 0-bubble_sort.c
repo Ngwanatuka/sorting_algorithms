ee4d1e7013cd6928e03a7db0b61dc02c8f581f7c
Ngwanatuka <Ngwanatuka@gmail.com>
2023-01-23 21:54:51 +0200

    task completed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,25 @@
 #include "sort.h"
-
 /**
- * bubble_sort - sorts an array of integers in ascending order
- * using the bubble sort algorithm
- * @array: input array
- * @size: size of array
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
  */
 void bubble_sort(int *array, size_t size)
 {
-  size_t i, j, temp = 0;
 
-  for (i = 0; i < size - 1; i++)
-  {
-    for  (j = 0; j < size - i - 1; j++)
-    {
-      if (array[j] > array[ j + 1])
-      {
-        temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-        /* print array after each swap */
-        print_array(array, size);
-      }
-    }
-  }
+	size_t i, index, tmp = 0;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size; i++)
+		for (index = 0; index < size; index++)
+		{
+			if (array[index] > array[index + 1] && array[index + 1])
+			{
+			tmp = array[index];
+			array[index] = array[index + 1];
+			array[index + 1] = tmp;
+			print_array(array, size);
+			}
+		}
 }
