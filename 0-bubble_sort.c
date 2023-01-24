cee79e719a26fdb4c605179ab46f6a11f3341581
Ngwanatuka <Ngwanatuka@gmail.com>
2023-01-24 10:28:13 +0200

    fixing code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,20 +8,25 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    int temp; i; j;
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                /* swap the elements */
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                /* print the array after each swap */
-                print_array(array, size);
-            }
-        }
-    }
+	int temp;
+	size_t i, j;
+
+  if (size < 2)
+    return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/*swap the elements */
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				/* print the array after each swap */
+				print_array(array, size);
+			}
+		}
+	}
 }
